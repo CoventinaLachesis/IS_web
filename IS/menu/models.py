01b5d0f327ad25007e289b56a17596f05c7b16ca
CoventinaLachesis <mark7416@hotmail.com>
2022-10-29 14:38:38 +0700

    add addmenu (not test)

--- IS/menu/models.py
+++ IS/menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    type = models.CharField(max_length=20)
+    img = models.ImageField(upload_to='images/', default=None)
+    
+    
+class Table_Order(models.Model):
+    table=models.IntegerField()
+    order= models.CharField()
--- IS/menu/views.py
+++ IS/menu/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from IS.menu.forms import MenuForm
+
+
+# Create your views here.
 
 def order(request,table):
     if request.method == 'POST':
+        
+        
+        
         return render(request,"menu/order_succes.html")
     
     return render(request,"menu/order.html")
@@ -10,4 +17,14 @@ def order(request,table):
 def alltable(request):
     
     return render(request,"menu/alltable.html")
-# Create your views here.
+
+
+def addmenu(request):
+    if request.method=='POST':
+        menu=MenuForm(request.get["name"],request.get["price"],request.get["type"],request.FILES)
+        if menu.is_valid():
+            menu.save()
+            
+            return render(request,"menu/addmenu.html")
+        
+    return render(request,"menu/addmenu.html")
